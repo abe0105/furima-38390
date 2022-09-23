8d32cc7eba7e283a45e8acc6cefa59e2f0d09e2e
abe0105 <abe.seiya0105@gmail.com>
2022-09-23 17:23:46 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname,           presence: true
+         validates :password,           format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+         validates :encrypted_password, presence: true
+         validates :first_name,         presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+         validates :last_name,          presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+         validates :kana_first_name,    presence: true,format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } 
+         validates :kana_last_name,     presence: true,format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } 
+         validates :birthday,           presence: true
+
 end
