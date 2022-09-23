ab73193860d5d232950b8e7121cedaf31a2d94b5
abe0105 <abe.seiya0105@gmail.com>
2022-09-23 21:26:55 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
          validates :nickname,           presence: true
          validates :password,           format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
          
-         validates :first_name,         presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-         validates :last_name,          presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+         validates :first_name,         presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message: 'Full-width characters' }
+         validates :last_name,          presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message: 'Full-width characters' }
          validates :kana_first_name,    presence: true,format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } 
          validates :kana_last_name,     presence: true,format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } 
          validates :birthday,           presence: true
