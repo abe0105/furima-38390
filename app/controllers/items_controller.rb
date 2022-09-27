6060cd8742e155daa01fc85a7c249979cdc3d709
abe0105 <abe.seiya0105@gmail.com>
2022-09-27 17:45:49 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,32 @@
 class ItemsController < ApplicationController
 
+  before_action :require_login, only: :new, alert: 'You need to sign in or sign up before continuing.'
+
   def index
+    @items = Item.all
   end
 
-end
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+    private
+  
+    def require_login
+      redirect_to user_session_path, alert: 'You need to sign in or sign up before continuing.' unless user_signed_in?
+    end
+  
+    def item_params
+      params.require(:item).permit(:image, :name, :explanation, :category_id, :commodity_condition_id, :shipping_charges_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+    end
+  end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,28 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+
+  has_one_attached :image
+
+
+
+    validates :image,                  presence: true
+    validates :name,                    presence: true
+    validates :explanation,             presence: true
+    validates :category_id,            presence: true
+    validates :commodity_condition_id,  presence: true
+    validates :shipping_charges_id,     presence: true
+    validates :prefecture_id,           presence: true
+    validates :days_to_ship_id,         presence: true 
+    validates :price,                   presence: true
+
+
+    validates :category_id,             numericality: { other_than: 0, message: 'select' }
+    validates :commodity_condition_id,  numericality: { other_than: 0, message: 'select' }
+    validates :shipping_charges_id,     numericality: { other_than: 0, message: 'select' }
+    validates :prefecture_id,           numericality: { other_than: 0, message: 'select' }
+    validates :days_to_ship_id,         numericality: { other_than: 0, message: 'select' }
+
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'Price Half-width number' }, inclusion: { in: (300..9_999_999), message: 'Out of setting range' }
+
 end
